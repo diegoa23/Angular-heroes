d7b6f7689466088fbb86802a5d48cada4d194e22
Diego Ayala <namadian13@gmail.com>
2025-03-31 10:33:50 -0400

    agregar

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,35 @@
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+export const canActivateGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  console.log('CanActivate');
+  console.log({ route, state });
+
+  return true;
+};
+
+export const canMatchGuard: CanMatchFn = (route: Route, segments: UrlSegment[]) => {
+  console.log('CanMatch');
+  console.log({ route, segments });
+
+  return true;
+};
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication()
+  .pipe(
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) {
+        router.navigate(['./']);
+      }
+    }),
+    map( isAuthenticated => !isAuthenticated )
+  );
+
+};
